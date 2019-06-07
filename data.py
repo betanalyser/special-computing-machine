770d15a689024402daab27c60c9c59ba2086906d
tryash <51205626+tryash@users.noreply.github.com>
2019-06-08 00:06:42 +0300

    Selecting sports types
     - Selecting types of supported sports
     - Simple "database" integration

--- data.py
+++ data.py
@@ -3,4 +3,19 @@ WELCOME_MESSAGE = """Hello, {}!
 In this bot you will be able to receive events suitable for your coefficients in sports!"""
 WHAT_SPORTS_INTEREST_YOU = 'What sports interest you?'
 DONE = 'Done'
+ON_CHOOSING_SPORT = 'Ok. Something else?'
 supported_sport_events = ['Football', 'Basketball', 'Hockey']
+"""
+Format
+{
+	'Chat id': {
+		'State': None,
+		'Selected sports': 
+			{
+			'Sport name: None
+			},
+		'Entered coefficients': [None, None]
+	}
+}
+"""
+users = {}
--- main.py
+++ main.py
@@ -3,35 +3,84 @@ import config
 import telebot
 from telebot import apihelper, types
 import proxy
-from data import WELCOME_MESSAGE, WHAT_SPORTS_INTEREST_YOU, DONE, supported_sport_events
+from data import WELCOME_MESSAGE, WHAT_SPORTS_INTEREST_YOU, DONE, ON_CHOOSING_SPORT
+from data import users, supported_sport_events
 apihelper.proxy = proxy.PROXY
 
 bot = telebot.TeleBot(config.token)
 
 
+def set_user(message):
+    user = {'State': 1,
+            'Selected sports':
+                {supported_sport_events[0]: False,
+                 supported_sport_events[1]: False,
+                 supported_sport_events[2]: False},
+            'Entered coefficients': [0.0, 0.0]
+            }
+    users[message.chat.id] = user
+
+
+def third_stage(message):
+    users[message.chat.id]['State'] = 3
+    pass
+
+
+def configurating_keyboard(message):
+    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+    for sport_name in supported_sport_events:
+        if users[message.chat.id]['Selected sports'][sport_name]:
+            sport = types.KeyboardButton(text='✅ {}'.format(sport_name))
+            keyboard.add(sport)
+        else:
+            sport = types.KeyboardButton(text=sport_name)
+            keyboard.add(sport)
+    button = types.KeyboardButton(text=DONE)
+    keyboard.add(button)
+    return keyboard
+
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
+    set_user(message)
     bot.send_message(message.chat.id,
                      WELCOME_MESSAGE.format(bot.get_chat(message.chat.id).first_name))
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     for sport_name in supported_sport_events:
-        sport = types.InlineKeyboardButton(text=sport_name)
+        sport = types.KeyboardButton(text=sport_name)
         keyboard.add(sport)
-    button = types.InlineKeyboardButton(text=DONE)
+    button = types.KeyboardButton(text=DONE)
     keyboard.add(button)
     bot.send_message(message.chat.id,
                      WHAT_SPORTS_INTEREST_YOU,
                      reply_markup=keyboard)
+    users[message.chat.id]['State'] = 2
 
 
 @bot.message_handler(content_types=['text'])
 def choosing_sports(message):
-    if message.text == supported_sport_events[0]:
-        print('test')
-    elif message.text == supported_sport_events[1]:
-        pass
-    elif message.text == supported_sport_events[2]:
-        pass
+    msg = message.text.replace('✅ ', '')
+    if msg == supported_sport_events[0]:
+        if not users[message.chat.id]['Selected sports'][supported_sport_events[0]]:
+            users[message.chat.id]['Selected sports'][supported_sport_events[0]] = True
+        else:
+            users[message.chat.id]['Selected sports'][supported_sport_events[0]] = False
+    elif msg == supported_sport_events[1]:
+        if not users[message.chat.id]['Selected sports'][supported_sport_events[1]]:
+            users[message.chat.id]['Selected sports'][supported_sport_events[1]] = True
+        else:
+            users[message.chat.id]['Selected sports'][supported_sport_events[1]] = False
+    elif msg == supported_sport_events[2]:
+        if not users[message.chat.id]['Selected sports'][supported_sport_events[2]]:
+            users[message.chat.id]['Selected sports'][supported_sport_events[2]] = True
+        else:
+            users[message.chat.id]['Selected sports'][supported_sport_events[2]] = False
+    if msg in supported_sport_events:
+        bot.send_message(message.chat.id,
+                         ON_CHOOSING_SPORT,
+                         reply_markup=configurating_keyboard(message))
+    if msg == DONE:
+        third_stage(message)
 
 
 if __name__ == '__main__':
