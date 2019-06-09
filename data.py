aa075dc0d2f8d3ad29897362006a87b6d0a68f89
tryash <51205626+tryash@users.noreply.github.com>
2019-06-09 17:12:09 +0300

    MVP
     - Debug
     - 2way and 3way sports support
     - New messages, keyboards for stages

--- data.py
+++ data.py
@@ -1,52 +1,72 @@
 # -*- coding: utf-8 -*-
-WELCOME_MESSAGE = """Hello, {}!
-In this bot you will be able to receive events suitable for your coefficients in sports!"""
-WHAT_SPORTS_INTEREST_YOU = 'What sports interest you?'
+WELCOME_MESSAGE = """Hello, {} 👋!
+In this bot you will be able to receive sport events suitable for coefficients that you are interested in! 🎲"""
+WHAT_SPORTS_INTEREST_YOU = 'What sports interest you? 🎯'
 DONE = 'Done'
-ON_CHOOSING_SPORT = 'Ok. Something else?'
-GO_TO_THE_EVENT_PAGE = 'Go to the event page'
-OK_TEXT_COEFFICIENTS_1 = """OK. Let us know with which coefficients events you are looking for.
-Text them one by one in the 1.01 format."""
-OK_TEXT_COEFFICIENTS_2 = 'Done. Text second coefficient.'
-EXCELLENT_SEARCHING = 'Excellent! Now we are going to look for suitable events and then we will send them you.'
-supported_sport_events = ['Football ⚽️', 'Basketball 🏀', 'Hockey 🏒']
+CANCEL = 'Cancel'
+RESET_PARAMETERS = 'Reset parameters'
+CHECKED_EMOJI = '✅ '
+DOES_NOT_MATTER = 'Does not matter'
+POSITIVE_INFINITY = 'inf'
+NEGATIVE_INFINITY = '-inf'
+ON_CHOOSING_SPORT = 'Ok. Something else? 😼'
+INVALID_SPORT_CHOOSING = 'You need to choose at least one kind of sport.'
+GO_TO_THE_EVENT_PAGE = 'Go to the event page 🔗'
+OK_TEXT_COEFFICIENTS_1 = """OK. Let us know with which coefficients events you are looking for. 👀
+Text them one by one in the __1.23__ format."""
+OK_TEXT_COEFFICIENTS_2 = 'Done. Text the second coefficient.'
+INVALID_COEFFICIENTS = 'Invalid coefficient. Try again.'
+DO_NOT_WORRY = 'No matching events found. Do not worry, the database is updated every 5 minutes. ⏰'
+EXCELLENT_SEARCHING = """Excellent! Now we are going to look for suitable events and then we will send them you.
+Note that the database is updated every 5 minutes ⏰.
+
+*Your searching parameters 🧮*
+Kinds of sport: {sport_types}
+Min coefficient: {min_coeff}
+Max coefficient: {max_coeff}"""
+supported_sport_events_emoji = {'Football': '⚽', 'Basketball': '🏀', 'Hockey': '🏒'}
+supported_sport_events = ['Football {}'.format(supported_sport_events_emoji['Football']),
+                          'Basketball {}'.format(supported_sport_events_emoji['Basketball']),
+                          'Hockey {}'.format(supported_sport_events_emoji['Hockey']
+                                             )]
 MINUTE = 1000 * 60
 five_min_delay = MINUTE * 5
-"""
-Format
-{
-	'ChatId': {
-		'State': None,
-		'SelectedSports': 
-			{
-			'SportName: None
-			},
-		'EnteredCoefficients': [None, None]
-	}
-}
-"""
 users = {}
-supported_sport_events_ru_en = {
-	'Футбол': supported_sport_events[0],
-	'Баскетбол': supported_sport_events[1],
-	'Хоккей': supported_sport_events[2]
-}
-event_message_template = """*{sport_type}*
-{competition}
+supported_sport_events_ru_en = {'Футбол': supported_sport_events[0],
+                                'Баскетбол': supported_sport_events[1],
+                                'Хоккей': supported_sport_events[2]}
+event_message_template_3_way = """{sport_type} *{competition}*
 {home_name} ({home_abbr}) — {away_name} ({away_abbr})
 
-*Coefficients*
-Chance of winning {home_name}: {home_chance}
-Chance of winning {away_name}: {away_chance}
-Chance of draw {away_name}: {draw_chance}
+*Coefficients* 🧮
+Win {home_name}: {home_chance}
+Win {away_name}: {away_chance}
+Draw: {draw_chance}
 
-*Weights*
-Weight of the {home_name} in the tournament table: {home_weight}
-Weight of the {away_name} in the tournament table: {away_weight}
+*Weights on the tournament table* 🎚
+{home_name}: {home_weight}
+{away_name}: {away_weight}
 
-*Suggestion*
+*Suggestion* 💰
 Most likely to win: {winner_name}
 Coefficient: {winner_coefficient}
 
-Event start date: {event_time}
+📅 Event start date: {event_time}
+"""
+event_message_template_2_way = """{sport_type} *{competition}*
+{home_name} ({home_abbr}) — {away_name} ({away_abbr})
+
+*Coefficients* 🧮
+Chance of winning {home_name}: {home_chance}
+Chance of winning {away_name}: {away_chance}
+
+*Weights on the tournament table* 🎚
+{home_name}: {home_weight}
+{away_name}: {away_weight}
+
+*Suggestion* 💰
+Most likely to win: {winner_name}
+Coefficient: {winner_coefficient}
+
+📅 Event start date: {event_time}
 """
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import config
 import telebot
 from telebot import apihelper, types
 import data
-from fonbet import actual_outcomes
+from fonbet import get_suitable_events
 from datetime import datetime
 import pickle
 from apscheduler.schedulers.background import BackgroundScheduler
@@ -34,39 +34,62 @@ def send_events(chat_id, events):
             url_button = types.InlineKeyboardButton(text=data.GO_TO_THE_EVENT_PAGE, url=event['EventLink'])
             keyboard.add(url_button)
             bot.send_message(chat_id, event['Message'], reply_markup=keyboard, parse_mode='Markdown')
-        except ConnectionError as e:
-            raise e
+        except ConnectionError as exception:
+            raise exception
 
 
-def parse_events():
-    events = actual_outcomes()
-    for event in events:
-        for user in data.users:
-            messages_for_send = []
-            if data.users[user]['State'] == 4 and any(data.users[user]['SelectedSports'].values()):
-                if (data.users[user]['SelectedSports'][data.supported_sport_events_ru_en[event['sport']]] and
-                        event['winner']['side'] is not None and
-                        data.users[user]['EnteredCoefficients'][0]
-                        < event['winner']['odds'] <
-                        data.users[user]['EnteredCoefficients'][1]):
-                    current_message = data.event_message_template.format(
-                        sport_type=data.supported_sport_events_ru_en[event['sport']],
-                        competition=translator.translate(event['competition']),
-                        home_name=translator.translate(event['home']['name']),
-                        home_abbr=translator.translate(event['home']['abbr']),
-                        away_name=translator.translate(event['away']['name']),
-                        away_abbr=translator.translate(event['away']['abbr']),
-                        home_chance=event['odds']['home']['value'],
-                        away_chance=event['odds']['away']['value'],
-                        draw_chance=event['odds']['draw']['value'],
-                        home_weight=event['home']['weight'],
-                        away_weight=event['away']['weight'],
-                        winner_name=translator.translate(event[event['winner']['side']]['name']),
-                        winner_coefficient=event['winner']['odds'],
-                        event_time=datetime.utcfromtimestamp(event['start']).strftime('%H:%M %d/%m/%Y'),
-                        )
+def parse_events(for_all=True, message=''):
+    if for_all:
+        dbase = data.users
+    else:
+        dbase = {message.chat.id: data.users[message.chat.id]}
+    for user in dbase:
+        messages_for_send = []
+        if data.users[user]['State'] == 4:
+            for event in get_suitable_events(coeff_min=data.users[user]['EnteredCoefficients'][0],
+                                             coeff_max=data.users[user]['EnteredCoefficients'][1],
+                                             sport_kinds=[sport[0].split(' ')[0]
+                                                          for sport in data.users[user]['SelectedSports'].items()
+                                                          if sport[1]]):
+                if event['winner']['side'] is not None:
+                    if event['odds']['type'] == '3way':
+                        current_message = data.event_message_template_3_way.format(
+                                sport_type=data.supported_sport_events_emoji[event['sport']],
+                                competition=event['competition'],
+                                home_name=event['home']['name'],
+                                home_abbr=event['home']['abbr'],
+                                away_name=event['away']['name'],
+                                away_abbr=event['away']['abbr'],
+                                home_chance=event['odds']['home']['value'],
+                                away_chance=event['odds']['away']['value'],
+                                draw_chance=event['odds']['draw']['value'],
+                                home_weight=event['home']['weight'],
+                                away_weight=event['away']['weight'],
+                                winner_name=event[event['winner']['side']]['name'],
+                                winner_coefficient=event['winner']['odds'],
+                                event_time=datetime.utcfromtimestamp(event['start']).strftime('%H:%M %d/%m/%Y'),
+                                )
+                    else:
+                        current_message = data.event_message_template_2_way.format(
+                                sport_type=data.supported_sport_events_emoji[event['sport']],
+                                competition=event['competition'],
+                                home_name=event['home']['name'],
+                                home_abbr=event['home']['abbr'],
+                                away_name=event['away']['name'],
+                                away_abbr=event['away']['abbr'],
+                                home_chance=event['odds']['home']['value'],
+                                away_chance=event['odds']['away']['value'],
+                                home_weight=event['home']['weight'],
+                                away_weight=event['away']['weight'],
+                                winner_name=event[event['winner']['side']]['name'],
+                                winner_coefficient=event['winner']['odds'],
+                                event_time=datetime.utcfromtimestamp(event['start']).strftime('%H:%M %d/%m/%Y'),
+                                )
                     messages_for_send.append({'Message': current_message, 'EventLink': event['link']})
-            send_events(user, messages_for_send)
+            if not for_all and len(messages_for_send) == 0:
+                bot.send_message(message.chat.id, data.DO_NOT_WORRY)
+            else:
+                send_events(user, messages_for_send)
 
 
 def set_user(message):
@@ -99,36 +122,41 @@ def second_stage(message):
         bot.send_message(message.chat.id,
                          data.WHAT_SPORTS_INTEREST_YOU,
                          reply_markup=keyboard)
-    except ConnectionError as e:
-        raise e
+    except ConnectionError as exception:
+        raise exception
 
 
 def third_stage(message):
     data.users[message.chat.id]['State'] = 3
-    keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    cancel = types.KeyboardButton(text='Cancel')
-    keyboard.add(cancel)
     try:
         bot.send_message(message.chat.id,
                          data.OK_TEXT_COEFFICIENTS_1,
-                         reply_markup=keyboard)
-    except ConnectionError as e:
-        raise e
+                         reply_markup=coefficient_keyboard(),
+                         parse_mode='Markdown')
+    except ConnectionError as exception:
+        raise exception
 
 
 def fourth_stage(message):
     data.users[message.chat.id]['State'] = 4
+    data.users[message.chat.id]['EnteredCoefficients'] = sorted(data.users[message.chat.id]['EnteredCoefficients'])
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-    reset = types.KeyboardButton(text='Reset parameters')
+    reset = types.KeyboardButton(text=data.RESET_PARAMETERS)
     keyboard.add(reset)
     try:
         bot.send_message(message.chat.id,
-                         data.EXCELLENT_SEARCHING,
-                         reply_markup=keyboard)
-    except ConnectionError as e:
-        raise e
-    data.users[message.chat.id]['EnteredCoefficients'] = sorted(data.users[message.chat.id]['EnteredCoefficients'])
-    parse_events()
+                         data.EXCELLENT_SEARCHING
+                         .format(
+                             sport_types=', '.join([sport[0] for sport in
+                                                    data.users[message.chat.id]['SelectedSports'].items() if sport[1]]),
+                             min_coeff=data.users[message.chat.id]['EnteredCoefficients'][0],
+                             max_coeff=data.users[message.chat.id]['EnteredCoefficients'][1]
+                         ),
+                         reply_markup=keyboard,
+                         parse_mode='Markdown')
+    except ConnectionError as exception:
+        raise exception
+    parse_events(for_all=False, message=message)
 
 
 def flip_sport_type(message, sport_name):
@@ -149,12 +177,13 @@ def check_coefficient(coefficient):
 def set_schedulers():
     scheduler = BackgroundScheduler()
     scheduler.add_job(
-        func=parse_events(),
+        func=dump_users_data,
+        args=[data.users],
         trigger='interval',
         minutes=5
     )
     scheduler.add_job(
-        func=dump_users_data(data.users),
+        func=parse_events,
         trigger='interval',
         minutes=5
     )
@@ -164,7 +193,7 @@ def configurating_keyboard(message):
     keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
     for sport_name in data.supported_sport_events:
         if data.users[message.chat.id]['SelectedSports'][sport_name]:
-            sport = types.KeyboardButton(text='✅ {}'.format(sport_name))
+            sport = types.KeyboardButton(text='{} {}'.format(data.CHECKED_EMOJI, sport_name))
             keyboard.add(sport)
         else:
             sport = types.KeyboardButton(text=sport_name)
@@ -174,74 +203,113 @@ def configurating_keyboard(message):
     return keyboard
 
 
+def coefficient_keyboard():
+    keyboard = types.ReplyKeyboardMarkup(row_width=5)
+    n = 0
+    keyboard_row = []
+    for coefficient in range(10, 30, 2):
+        keyboard_row.append(types.KeyboardButton(text=str(coefficient/10)))
+        n += 1
+        if n % 5 == 0:
+            keyboard.row(keyboard_row[0], keyboard_row[1], keyboard_row[2], keyboard_row[3], keyboard_row[4])
+            keyboard_row = []
+            n = 0
+    keyboard.row(types.KeyboardButton(text=data.DOES_NOT_MATTER))
+    cancel = types.KeyboardButton(text=data.CANCEL)
+    keyboard.add(cancel)
+    return keyboard
+
+
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
     set_user(message)
     try:
         bot.send_message(message.chat.id,
                          data.WELCOME_MESSAGE.format(bot.get_chat(message.chat.id).first_name))
-    except ConnectionError as e:
-        raise e
+    except ConnectionError as exception:
+        raise exception
     second_stage(message)
 
 
 @bot.message_handler(content_types=['text'])
 def users_distribution(message):
-    if data.users[message.chat.id]['State'] == 2:
-        choosing_sports(message)
-    elif data.users[message.chat.id]['State'] == 3:
-        parse_coefficients(message)
-    elif data.users[message.chat.id]['State'] == 4:
-        events_streaming(message)
+    if message.chat.id in data.users:
+        if data.users[message.chat.id]['State'] == 2:
+            choosing_sports(message)
+        elif data.users[message.chat.id]['State'] == 3:
+            parse_coefficients(message)
+        elif data.users[message.chat.id]['State'] == 4:
+            events_streaming(message)
+    else:
+        set_user(message)
+        second_stage(message)
 
 
 def events_streaming(message):
-    if message.text == 'Reset parameters':
+    if message.text == data.RESET_PARAMETERS:
         reset_user_data(message)
         second_stage(message)
 
 
 def parse_coefficients(message):
-    if message.text == 'Cancel':
+    if message.text == data.CANCEL:
         reset_user_data(message)
         second_stage(message)
-    if check_coefficient(message.text):
-        if not any(data.users[message.chat.id]['EnteredCoefficients']):
-            data.users[message.chat.id]['EnteredCoefficients'][0] = float(message.text)
-            keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
-            cancel = types.KeyboardButton(text='Cancel')
-            keyboard.add(cancel)
+    else:
+        if check_coefficient(message.text) or message.text == data.DOES_NOT_MATTER:
+            msg = message.text
+            if not any(data.users[message.chat.id]['EnteredCoefficients']):
+                if msg == data.DOES_NOT_MATTER:
+                    msg = data.NEGATIVE_INFINITY
+                data.users[message.chat.id]['EnteredCoefficients'][0] = float(msg)
+                keyboard = types.ReplyKeyboardMarkup(row_width=1, resize_keyboard=True)
+                cancel = types.KeyboardButton(text=data.CANCEL)
+                keyboard.add(cancel)
+                try:
+                    bot.send_message(message.chat.id,
+                                     data.OK_TEXT_COEFFICIENTS_2,
+                                     reply_markup=coefficient_keyboard())
+                except ConnectionError as exception:
+                    raise exception
+            else:
+                if msg == data.DOES_NOT_MATTER:
+                    msg = data.POSITIVE_INFINITY
+                data.users[message.chat.id]['EnteredCoefficients'][1] = float(msg)
+                fourth_stage(message)
+        else:
             try:
                 bot.send_message(message.chat.id,
-                                 data.OK_TEXT_COEFFICIENTS_2,
-                                 reply_markup=keyboard)
-            except ConnectionError as e:
-                raise e
-        else:
-            data.users[message.chat.id]['EnteredCoefficients'][1] = float(message.text)
-            fourth_stage(message)
+                                 data.INVALID_COEFFICIENTS,
+                                 reply_markup=coefficient_keyboard())
+            except ConnectionError as exception:
+                raise exception
 
 
 def choosing_sports(message):
-    msg = message.text.replace('✅ ', '')
+    msg = message.text.replace(data.CHECKED_EMOJI, '')
     if msg == data.DONE:
-        third_stage(message)
+        if any(data.users[message.chat.id]['SelectedSports'].values()):
+            third_stage(message)
+        else:
+            bot.send_message(message.chat.id,
+                             data.INVALID_SPORT_CHOOSING,
+                             reply_markup=configurating_keyboard(message))
     else:
-        data.users[message.chat.id]['SelectedSports'][msg] = flip_sport_type(message, msg)
         if msg in data.supported_sport_events:
+            data.users[message.chat.id]['SelectedSports'][msg] = flip_sport_type(message, msg)
             try:
                 bot.send_message(message.chat.id,
                                  data.ON_CHOOSING_SPORT,
                                  reply_markup=configurating_keyboard(message))
-            except ConnectionError as e:
-                raise e
+            except ConnectionError as exception:
+                raise exception
 
 
 if __name__ == '__main__':
     while True:
         data.users = load_users_data()
-        # TODO set_schedulers()
+        set_schedulers()
         try:
             bot.polling(none_stop=True)
-        except ConnectionError:
-            print('Connection error')
+        except ConnectionError as e:
+            raise e
